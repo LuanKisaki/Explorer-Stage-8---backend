0666278185cb812e82b403ce7dc2f304d895c9c9
Luan Kisaki <luankisaki@gmail.com>
2023-11-22 16:26:15 -0300

    first commit

---
+++ src/routes/users.routes.js
@@ -0,0 +1,33 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/UsersController");
+
+const usersRoutes = Router();
+
+// Permite, nega, fiscaliza ou direciona uma request
+function Middleware(req, res, next) {
+
+  if (!req.body.isAdmin) {
+    return res.json({
+      message: "user unauthorized"
+    })
+  }
+
+  console.log("Autorizado pelo Middleware");
+  console.log(req.body);
+  next();
+}
+
+const usersController = new UsersController();
+// aplicar Middleware para todas as rotas
+usersRoutes.use(Middleware);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
+
+// usersRoutes.post("/", Middleware, usersController.create)
+// usersRoutes.post("/", Middleware, usersController.index)
+// usersRoutes.post("/", Middleware, usersController.show)
+// usersRoutes.post("/", Middleware, usersController.update)
+// usersRoutes.post("/", Middleware, usersController.delete)
+
+module.exports = usersRoutes;
